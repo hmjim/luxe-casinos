baaa38bf312875e1c111b2dfef7c315ec10b6463
Max <hmjim@mail.ru>
2022-02-01 22:44:20 +0700

    01.02

--- index.php
+++ index.php
@@ -173,9 +173,11 @@ curl_close( $ch );
 $bodytag = str_replace( "((?!vulkanumm\.com/b)\w+(?:\.\w+)+", "https://luxe-casinos.azurewebsites.net/", $contents );
 $result  = preg_replace( '~' . $new_url . '~m', "luxe-casinos.azurewebsites.net", $contents );
 preg_match_all( "/(https:\/\/vulkanumm.com).*\.(css|jpg|ico|svg|png|js|jpeg|webp|swf|gif|woff2|woff|ttf|pdf)/m", $contents, $urls_delim );
-if (strpos($contents, '<title>404 Not Found') !== false) {
+
+if (strpos($contents, 'Ой, это 404') !== false) {
 	header("HTTP/1.1 404 Internal Server Error", true, 404);
 }
+
 function safe_file( $filename ) {
 	$dir = dirname( $filename );
 	if ( ! file_exists( __DIR__ . $dir ) ) {
